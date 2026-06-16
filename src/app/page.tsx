d1ac689d7e01a643f6169da7093d693d57a0ebfd
Luke Piggott <luk3.piggott6@gmail.com>
2026-06-16 11:01:01 +0100

    Initialise state variables and define helper function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,20 @@
-export default async function Home() {
-  const url = 'https://pokeapi.co/api/v2/pokemon'
-  const res = await fetch(url);
-  const data = await res.json()
-  console.log(data);
+'use client';
+
+import { useState } from 'react';
+
+export default function Home() {
+
+  const [pokemonList, setPokemonList] = useState([]);
+  const [nextUrl, setNextUrl] = useState(null);
+  const [prevUrl, setPrevUrl] = useState(null);
+
+  const testFetch = async () => {
+    const url = 'https://pokeapi.co/api/v2/pokemon';
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data);
+  };
+
   return (
     <main className="p-8">
       <h1>Luke's Pokémon App</h1>
